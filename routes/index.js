9b2f5c67a4bdbd6b73a1ece68bcbb451a9757ea5
GerardoRegalado <gerardo.regalado24@gmail.com>
2022-01-31 12:09:45 -0600

    Ejercicio de 5min

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,21 @@
 // seccion 1. IMPORTACIONES
 
 const express  =require("express")
+const res = require("express/lib/response")
 const router = express.Router()
 
 
 // seccion 2. RUTEO
 router.get("/", (req, res) => {
     res.send("hola mundo")
+})
+
+router.get("/contacto", (req,res) => {
+    res.send("contacto")
+});
+
+router.get("/ejercicio", (req,rest) => {
+    res.send("ejercicio de 5min")
 })
 
 
